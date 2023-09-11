51c44b6825d2b87d03cfd48a75d32378e29ad882
Jon Haraldsvik <haraldsvik91@gmail.com>
2023-09-11 20:40:13 +0200

    some refactoring

--- index.ts
+++ index.ts
@@ -1,29 +1,52 @@
 const fetchFPL = async (url: string) => {
   try {
     const res = await fetch(url, { headers: { "User-Agent": "" } });
-    const data = await res.json();
-    return data;
+
+    if (!res.ok) {
+      throw new Error("Failed fetching FPL data.");
+    }
+
+    return await res.json();
   } catch (error) {
-    console.log("FPL error", error);
+    console.error("FPL error", error);
+    throw error;
   }
 };
+interface FPLSummary {
+  summary_overall_points: number;
+  summary_event_points: number;
+  summary_overall_rank: number;
+  id: number;
+  fplName: string;
+  teamName: string;
+  country: string;
+  countryCode: string;
+}
+const fetchSummary = async (entry: string): Promise<FPLSummary> => {
+  const fplData = await fetchFPL(`https://fantasy.premierleague.com/api/entry/${entry}/`);
 
-const fetchSummary = async (entry: string) => {
-  let fplData = await fetchFPL(
-    `https://fantasy.premierleague.com/api/entry/${entry}/`
-  );
+  const {
+    summary_overall_points,
+    summary_event_points,
+    summary_overall_rank,
+    id,
+    player_first_name,
+    player_last_name,
+    name,
+    player_region_name,
+    player_region_iso_code_long
+  } = fplData;
 
-  const data = {
-    summary_overall_points: fplData.summary_overall_points,
-    summary_event_points: fplData.summary_event_points,
-    summary_overall_rank: fplData.summary_overall_rank,
-    id: fplData.id,
-    fplName: `${fplData.player_first_name} ${fplData.player_last_name}`,
-    teamName: fplData.name,
-    country: fplData.player_region_name,
-    countryCode: fplData.player_region_iso_code_long,
+  return {
+    summary_overall_points,
+    summary_event_points,
+    summary_overall_rank,
+    id,
+    fplName: `${player_first_name} ${player_last_name}`,
+    teamName: name,
+    country: player_region_name,
+    countryCode: player_region_iso_code_long,
   };
-  return data;
 };
 
 const server = Bun.serve({
@@ -31,27 +54,41 @@ const server = Bun.serve({
   async fetch(req) {
     const url = new URL(req.url);
     console.log(JSON.stringify(url));
+
     if (url.pathname === "/") return new Response("Home page!");
+
     if (url.pathname === "/fpl") {
       try {
-        console.log("here");
         const query = new URLSearchParams(url.search);
-        const entry = query.get("entry") ?? "";
-        console.log(entry);
-        if (!entry)
-          return new Response(JSON.stringify({ msg: "invalid entry" }));
-        const data = await fetchSummary(entry);
+        const entry = query.get("entry");
 
+        if (!entry) {
+          return new Response(JSON.stringify({ msg: "invalid entry" }), {
+            status: 400,
+            headers: {
+              "content-type": "application/json",
+            },
+          });
+        }
+
+        const data = await fetchSummary(entry);
         return new Response(JSON.stringify(data), {
           headers: {
             "content-type": "application/json",
           },
         });
+
       } catch (err) {
-        return new Response("something went wrong :/" + JSON.stringify(err));
+        return new Response("something went wrong :/" + JSON.stringify(err), {
+          status: 500,
+          headers: {
+            "content-type": "application/json",
+          },
+        });
       }
     }
-    return new Response("404!");
+
+    return new Response("404!", { status: 404 });
   },
 });
 
